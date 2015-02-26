0e14757db63209fd33194f23b9b4a35017127d0f
Amirkhan <Amirkhan@Amirkhans-MacBook-Pro.local>
2015-02-27 03:58:24 +0500

    Initial Commit

---
+++ Noisli/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Noisli
+//
+//  Created by Amirkhan on 2/27/15.
+//  Copyright (c) 2015 Amirkhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Noisli/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Noisli
+//
+//  Created by Amirkhan on 2/27/15.
+//  Copyright (c) 2015 Amirkhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
